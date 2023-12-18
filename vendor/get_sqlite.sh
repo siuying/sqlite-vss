f4841a4db78070c468bbcb846014ea50d62f730d
Francis Chong <francis@goodnotes.com>
2023-12-18 19:32:53 +0000

    update sqlite version

--- vendor/get_sqlite.sh
+++ vendor/get_sqlite.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 VENDOR_DIR=$(dirname "$0")
 
-wget https://www.sqlite.org/2022/sqlite-autoconf-3400100.tar.gz -O $VENDOR_DIR/sqlite.tar.gz
+wget https://www.sqlite.org/2023/sqlite-autoconf-3440200.tar.gz -O $VENDOR_DIR/sqlite.tar.gz
 tar -xvzf $VENDOR_DIR/sqlite.tar.gz
 rm $VENDOR_DIR/sqlite.tar.gz
-mv sqlite-autoconf-3400100/ $VENDOR_DIR/sqlite
+mv sqlite-autoconf-3440200/ $VENDOR_DIR/sqlite
